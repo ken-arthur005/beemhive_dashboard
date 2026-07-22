6a9a799a3d3c1bcf2e9529b250ec383d4980b743
ken-arthur005 <careyckenarthur@gmail.com>
2026-07-22 22:23:55 +0000

    sadasg

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const params = await searchParams
+  const code = params?.code
+  if (code) {
+    const next = params?.next ?? '/invite'
+    redirect(`/auth/callback?code=${encodeURIComponent(code)}&next=${encodeURIComponent(next)}`)
+  }
   redirect('/login')
 }
